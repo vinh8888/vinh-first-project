d36076fb678bb9b5a341c89573f95be30a0c9914
vinh8888 <voquangvinh@gmail.com>
2022-09-12 17:58:08 +0700

    add 3 Constants, RestFB, LoginFacebookServlet
    description add 3 Constants, RestFB, LoginFacebookServlet

---
+++ stackjava/com/accessfacebook/common/Constants.java
@@ -0,0 +1,9 @@
+package stackjava.com.accessfacebook.common;
+
+public class Constants {
+	public static String FACEBOOK_APP_ID = "359123991240252";
+	public static String FACEBOOK_APP_SECRET = "d07e182d8495df6930665d6c39fbe8ac";
+	public static String FACEBOOK_REDIRECT_URL = "https://localhost:8443/AccessFacebook/login-facebook";
+	public static String FACEBOOK_LINK_GET_TOKEN = "https://graph.facebook.com/oauth/access_token?client_id=%s&client_secret=%s&redirect_uri=%s&code=%s";
+
+}
